c9e895e693844fe306bf94213beaf7365a631cdc
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:29:26 -0800

    stripping name
    Typescript 45 Q exercise 6

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let person_name = "\t    Anila Hunain \n";
+console.log("Name with white space:", person_name);
+let strippedName = person_name.trim();
+console.log("Stripped name:", strippedName);
